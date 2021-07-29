773d8d07b4755bac83847716d1c1172b286e44b1
Moses <katsande.moses@gmail.com>
2021-07-29 21:05:48 +0200

    Getting Started With JMS

---
+++ nxs-jms/src/test/java/com/nexstudio/nxsjms/NxsJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexstudio.nxsjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NxsJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
